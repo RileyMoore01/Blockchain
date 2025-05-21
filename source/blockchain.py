6485f59cef18083c775872fac58badc447c5b7e9
Riley Moore <50551379+RileyMoore01@users.noreply.github.com>
2025-05-21 09:21:01 -0500

    Update blockchain.py

--- source/blockchain.py
+++ source/blockchain.py
@@ -1,1 +1,5 @@
-
+# creates an intital empty list (to store our blockchain) and another to store transactions
+class Blockchain(object):
+  def __init__(self):
+    self.chain = []
+    self.current_transactions = []
