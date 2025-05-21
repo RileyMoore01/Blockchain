ccee4142a46e53d5e941cbaf2abdbb69eaa65482
Riley Moore <50551379+RileyMoore01@users.noreply.github.com>
2025-05-21 09:49:46 -0500

    Update blockchain.py

--- source/blockchain.py
+++ source/blockchain.py
@@ -3,3 +3,13 @@ class Blockchain(object):
   def __init__(self):
     self.chain = []
     self.current_transactions = []
+
+  def new_block(self):
+    pass # creates a new block and adds it to the chain
+
+  def new_transaction(self):
+    pass # add a new transaction to the list of transactions
+
+  @staticmethod
+  def hash(self):
+    pass #hashes a block
